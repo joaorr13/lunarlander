8166a9cf1ad2eaa797d9bc2996b2aa9d196f5208
joaorr13 <joaorolareis@gmail.com>
2021-01-07 20:10:58 +0000

    Implementação de limite de velocidade para a aterrar

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ class Lander(Sprite):
         self.move_right = False
         self.move_up = False
 
-        self.fuel = 1000
+        self.fuel = 500
 
     def move(self):
         self.rect = self.rect.move(self.speed)
@@ -41,16 +41,18 @@ class Lander(Sprite):
     def update(self):
         if self.move_left and self.rect.left > 0:
             if self.speed[0] > -2:
-                self.speed[0] -= 0.25
+                self.speed[0] -= 0.09
         if self.move_right and self.rect.right < self.screen_rect.right:
             if self.speed[0] < 2:
-                self.speed[0] += 0.25
+                self.speed[0] += 0.09
         if self.move_up and self.fuel > 0:
             self.fuel -= 1
             if self.speed[1] > -1.5:
-                self.speed[1] -= 0.25
-        if self.speed[1] < 2:
-            self.speed[1] += 0.18
+                self.speed[1] -= 0.09
+        if self.speed[1] < 3 and self.move_up == False:
+            self.speed[1] += 0.06
+        if self.speed[1] < 3 and self.move_up:
+            self.speed[1] += 0.05
         if self.rect.left <= 0 and self.move_left:
             self.speed[0] = 0
         if self.rect.right >= self.screen_rect.right and self.move_right:
@@ -99,10 +101,11 @@ class ScoreBoard:
         self.lander = Lander(ll_game)
 
         self.total_score = 0
-        self.total_fuel = 1000
+        self.total_fuel = 500
         self.wastefuel = False 
         self.is_gameover = False 
         self.is_gameover_clutch = False 
+        self.too_fast = False
         self.contador = 0
         
     def draw_fuel(self):
@@ -129,6 +132,13 @@ class ScoreBoard:
             self.score = self.settings.font.render('Score  ' + str(self.total_score + 1),1,self.settings.white)
             self.screen.blit(self.gameover,(self.settings.screen_width / 2 - self.gameover.get_width() / 2 , self.settings.screen_height / 2 - self.gameover.get_height() / 2 - 55))
             self.screen.blit(self.score, (self.settings.screen_width / 2 - self.score.get_width() / 2 , self.settings.screen_height / 2 - self.score.get_height() / 2 + 7))
+
+    def draw_speed(self, ll_speed):
+        self.speed = self.settings.font.render('Vertical Speed  ' + str(int(ll_speed *65.7)),1, self.settings.white)
+        self.screen.blit(self.speed,(1080 - self.speed.get_width(),0))
+        if self.too_fast:
+            self.too_fast_text = self.settings.font.render('Too Fast!',1, self.settings.white)
+            self.screen.blit(self.too_fast_text, (self.settings.screen_width / 2 - self.too_fast_text.get_width() / 2 , self.settings.screen_height / 2 - self.too_fast_text.get_height() / 2 - 55))
 
 class LunarLander:
     'Overall class to manage game assets and behavior.'
@@ -206,13 +216,17 @@ class LunarLander:
         landing = pygame.sprite.collide_mask(self.lander, self.base)
         if landing:
             if self.scoreboard.total_fuel > 0:
-                self.lander.speed = [0, 0]
                 self.scoreboard.contador += 1 
                 if self.scoreboard.contador == 1:
-                    self.scoreboard.total_score += 1 
+                    if self.lander.speed[1] < 125 / 65.7:
+                        self.scoreboard.total_score += 1
+                    else: 
+                        self.scoreboard.too_fast = True 
+                self.lander.speed = [0, 0]
                 if self.scoreboard.contador == 50:
                     self.lander.rect = self.lander.image.get_rect()
                     self.scoreboard.contador = 0
+                    self.scoreboard.too_fast = False 
             else:
                 self.lander.speed = [0,0]
                 self.scoreboard.is_gameover_clutch =  True
@@ -226,6 +240,7 @@ class LunarLander:
         self.scoreboard.draw_fuel()
         self.scoreboard.draw_score()
         self.scoreboard.draw_gameover()
+        self.scoreboard.draw_speed(self.lander.speed[1])
 
         pygame.display.flip()
         pygame.time.delay(5)
